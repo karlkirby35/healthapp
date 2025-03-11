13e89fed00d4eefcb4ff5c53db52104d00c21ef6
Karl Kirby (Student) <N00235865@iadt.ie>
2025-03-11 12:56:57 +0000

    today

--- app/Http/Controllers/NutritionixController.php
+++ app/Http/Controllers/NutritionixController.php
@@ -7,34 +7,29 @@ use GuzzleHttp\Client;
 
 class NutritionixController extends Controller
 {
-    public function getNutritionData(Request $request)
-    {
-        $foodName = $request->input('food', 'apple'); // Default to 'apple' if not provided
+    public function getNutrition()
+    { 
         
-        // Get API credentials from .env
-        $appId = env('NUTRITIONIX_APP_ID');
-        $appKey = env('NUTRITIONIX_APP_KEY');
-
+        $apiKey = '5033eff289ba63af644933b5a6229ccd';
+        
+      
         $client = new Client();
 
-        try {
-            $response = $client->request('GET', 'https://api.nutritionix.com/v1_1/search/' . $foodName, [
-                'query' => [
-                    'results' => '0:20',
-                    'fields' => 'item_name,item_id,brand_name,nf_calories,nf_total_fat',
-                ],
-                'headers' => [
-                    'x-app-id' => $appId,
-                    'x-app-key' => $appKey,
-                ]
-            ]);
+   
+        $apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid={$apiKey}";
 
-            $data = json_decode($response->getBody()->getContents(), true);
-            
-            // Pass the results to the view
-            return view('nutritionix', ['foods' => $data['hits']]);
+        try {
+           
+            $response = $client->get($apiUrl);
+
+      
+            $data = json_decode($response->getBody(), true);
+
+     
+            return view('weather', ['weatherData' => $data]);
         } catch (\Exception $e) {
-            return response()->json(['error' => 'Unable to fetch data from Nutritionix API'], 500);
+ 
+            return view('api_error', ['error' => $e->getMessage()]);
         }
     }
 }
